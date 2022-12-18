d0b931201f429ef8ae53ba2c47e84df4963b42a8
Battula B Hemalatha <119840342+hemalathabattula26@users.noreply.github.com>
2022-12-18 21:50:44 +0530

    #include<stdio.h> int main() {     int a,b,i,c,n,n2,n3;     scanf("%d%d",&a,&b);     for(i=a+1;i<b;i++)     {         n=i;         n2=i*i;         n3=i*i*i;         printf("%d %d %d ",n,n2,n3);     } }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,c,n,n2,n3;
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        n=i;
+        n2=i*i;
+        n3=i*i*i;
+        printf("%d %d %d
+",n,n2,n3);
+    }
+}
