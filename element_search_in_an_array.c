2b3fef760407af9f0b86ad35ef564195bce682d7
Battula B Hemalatha <119840342+hemalathabattula26@users.noreply.github.com>
2023-01-03 11:39:16 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int m;     scanf("%d",&m);     for(i=0;i<n;i++)     {         if(m==a[i])         {             c++;         }     }     if(c>=1) printf("True");     else printf("False"); }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int m;
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        if(m==a[i])
+        {
+            c++;
+        }
+    }
+    if(c>=1) printf("True");
+    else printf("False");
+}
