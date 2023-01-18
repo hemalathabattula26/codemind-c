28ea70fdc5559aa980bce9b16dce186eb6d18919
Battula B Hemalatha <119840342+hemalathabattula26@users.noreply.github.com>
2023-01-18 22:26:22 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int n,i,q,r,s=0;         scanf("%d",&n);         q=n;         while(q!=0)         {             r=q%10;             s=s*10+r;              q=q/10;         }         if(s==n)         {             printf("True ");         }         else         {             printf("False ");         }     } }

---
+++ Palindrome_Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,i,q,r,s=0;
+        scanf("%d",&n);
+        q=n;
+        while(q!=0)
+        {
+            r=q%10;
+            s=s*10+r;
+             q=q/10;
+        }
+        if(s==n)
+        {
+            printf("True
+");
+        }
+        else
+        {
+            printf("False
+");
+        }
+    }
+}
