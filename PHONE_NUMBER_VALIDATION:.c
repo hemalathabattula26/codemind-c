48c3165ad843775d64477fc028398720c454b035
Battula B Hemalatha <119840342+hemalathabattula26@users.noreply.github.com>
2023-01-20 20:40:26 +0530

    #include<stdio.h> int main() {     long long int n;     int c=0;     scanf("%lld",&n);     while(n>0)     {         c++;         n=n/10;     }     if(c==10)     {         printf("Valid");     }     else     {         printf("Invalid");     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    long long int n;
+    int c=0;
+    scanf("%lld",&n);
+    while(n>0)
+    {
+        c++;
+        n=n/10;
+    }
+    if(c==10)
+    {
+        printf("Valid");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+}
